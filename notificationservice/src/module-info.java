a4736b6b1ecd988cea7e63910fe38eca6ad2ab19
subarna <khadka.subarnabijaya@gmail.com>
2024-09-29 16:22:49 -0400

    notification service

--- notificationservice/src/module-info.java
+++ notificationservice/src/module-info.java
@@ -1,4 +1,7 @@
 module notificationservice {
+    // exports notification service package to be used by other modules
     exports com.notificationservice;
+
+    // This module uses this service (Notice the full path of the service)
     uses com.notificationservice.NotificationService;
 }
