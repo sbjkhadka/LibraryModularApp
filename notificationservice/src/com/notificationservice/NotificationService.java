217f5b16f1b2ef0418876a6196b5f28d1d32e836
subarna <khadka.subarnabijaya@gmail.com>
2024-09-29 01:32:17 -0400

    simple comments

--- notificationservice/src/com/notificationservice/NotificationService.java
+++ notificationservice/src/com/notificationservice/NotificationService.java
@@ -1,6 +1,7 @@
 package com.notificationservice;
 
 public interface NotificationService {
+    // This is an abstract of any notification so user can implement it accordingly
     String getName();
     void sendNotification(String message);
 }
